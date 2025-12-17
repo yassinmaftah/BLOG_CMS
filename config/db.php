21b26a370e5086f1c449aabf416f1a5a568dc689
yassinmftah <yassinmaftah06@gmail.com>
2025-12-17 09:24:37 +0100

    connect database with ph

--- config/db.php
+++ config/db.php
@@ -3,10 +3,10 @@
 $host = 'localhost';
 $dbname = 'blog';
 $user = 'root';
-$pass = 'sqlyassine2025'; 
+$pass = '*************'; 
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
